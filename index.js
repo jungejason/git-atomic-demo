40e2824b7d8aabc39e06cc1c3654e41dfb606f4d
Jason Ge <gejun_1978@yahoo.com>
2019-10-14 12:52:13 +0800

    Add a new endpoint to return timestamp
    Summary:
    AT
    
    Test:
    
        ╰─ http localhost:3000/timestamp
        HTTP/1.1 200 OK
        Connection: keep-alive
        Content-Length: 13
        Content-Type: text/html; charset=utf-8
        Date: Mon, 14 Oct 2019 03:51:02 GMT
        ETag: W/"d-4Y4rjNxNJ+YoUBWdyrnU4aBUWNU"
        X-Powered-By: Express
    
        1571025062045

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.get('/timestamp', function (req, res) {
+  res.send('' + Date.now())
+})
+
 app.get('/', function (req, res) {
   res.send('hello world')
 })
