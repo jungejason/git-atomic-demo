7db082ab0f105ea185c89a0ba691857b55566469
Jason Ge <gejun_1978@yahoo.com>
2019-10-14 12:50:36 +0800

    Change magic port AND add a endpoint
    Summary:
    It's not good to have a magic number. This commit changes it to a
    varaible.
    Also added a endpoint
    
    Test:
    none

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+var port = 3000
 var express = require('express')
 var app = express()
+
+app.get('/timestamp', function (req, res) {
+  res.send('' + Date.now())
+})
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(3000)
+// Start the server
+app.listen(port)
